7f67afeb6309c6c311a8797678f21e331985b9c9
Poly <rodriguezpaula606@gmail.com>
2021-03-25 17:54:08 -0500

    view general and form add house

--- views/GeneralLessor.php
+++ views/GeneralLessor.php
@@ -5,12 +5,62 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>¡House rent Armenia!</title>
+    <link rel="stylesheet"  href="../assests/css/banerLessor.css">
 </head>
 <body>
+    <!--Navbar-->
     <?php
     include("../views/layouts/NavbarLessor.php");
     ?>
-    <h1>Infromación general de la app</h1>
+    <!--Slider-->
+    </br>
+    <div class="banner">
+		<div class="banner-content">
+			<h1>¡House rent Armenia!</h1>
+			<h2>¡Descubre en qué consiste ser anfitrión!</h2>
+
+		</div>
+	</div>
+        </br>
+        <!--info-->
+    <div class="information">
+        <h1>Descubre en qué consiste ser anfitrión</h1>
+        <div class="reasons">
+            <div>
+                <h3>Razones para convertirte en anfitrión</h3>
+                <p>Sean cuales sean tus objetivos financieros, ser anfitrión en Armenia pone a tu disposición una forma única de alcanzarlos. Destina lo que ganes a pagar la hipoteca, a mejorar tu alojamiento o, simplemente, regálate unas merecidas vacaciones. Abrir las puertas de tu hogar ayudará a que tu cuenta crezca, pero también te aportará mucho más. Según los anfitriones de nuestra comunidad, una de las ventajas es que puedes compartir tu cultura y conocer a personas con las que si no nunca habrías coincidido, como músicos profesionales, fabricantes de drones o artistas de circo.</p>
+            </div>
+
+            <div class="Circle">
+            </div>
+        </div>
+    </div>
+    <div class="protection">
+        <h3>Tips para protejer a los inivitados</h3>
+        <h4>Prácticas de seguridad contra el COVID-19</h4>
+        <div class="Tips">
+            <div>
+                </br>
+                <div class="Circle-two"></div>
+                <div class="Circle-tree"></div>
+                <div class="Circle-four"></div>
+            </div>
+
+            <div>
+                <h5>Paso 1: Preparar</h5>
+                <p>Una preparación adecuada puede ayudarte a ti y a tu equipo a limpiar de manera más eficiente y segura.</p>
+                <h5>Paso 2: Limpiar.</h5>
+                <p>Es importante comenzar por eliminar el polvo y la suciedad de las superficies, como los pisos y las barras.</p>
+                <h5>Paso 3: Sanitizar.</h5>
+                <p> Utiliza desinfectantes químicos para ayudar a reducir la cantidad de bacterias en superficies de alto contacto, como pomos de puertas y controles remotos.</p>
+                <h5>Paso 4: Verificar.</h5>
+                <p>Consulta la lista de verificación de cada habitación para conocer las mejores prácticas sobre cómo limpiar y sanitizar diferentes espacios.</p>
+                <h5>Paso 5: Reponer.</h5>
+                <p>Para evitar la contaminación cruzada, es importante que termines de limpiar e sanitizar una habitación y te laves las manos antes de reponer los artículos para recibir al próximo huésped.</p>
+            </div>
+        </div>
+    </div>
+    <!--Footer-->
 
 </body>
 </html>
