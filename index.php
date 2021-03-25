58346ccc25bc4dd6cdffcfebc91bebc02bcb4406
Juliana Fajardo <juliana.fajardo92@gmail.com>
2021-03-25 16:48:16 -0500

    index250321

--- index.php
+++ index.php
@@ -1,202 +1,149 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <title>¡CASAS EN RENTA ARMENIA!</title>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>¡House rent Armenia!</title>
+
     <!-- BOOTSTRAP 4 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <!-- CSS STYLES INDEX -->
-    <link rel="stylesheet" type="text/css" href="assests/css/styles.css">
+    <!-- FONT AWESOEM -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="assests/css/styles.css">
+
 </head>
 
 <body>
-    <!-- HEADER AND MENU -->
     <header>
-        <nav class="navbar navbar-light">
-
+        <nav class="navbar">
             <div class="container">
-                <img class="logo" src="assests/img/logo.png">
+                <a class="navbar-brand" href="../index.php">
+                    <img class="logo" src="./assests/img/hotel2.png" />
+                </a>
                 <ul class="menu">
                     <li><a href="#">HOME</a></li>
                     <li><a href="#">HOUSES</a></li>
-
+                    <li><a href="#">CONTAC</a></li>
                     <li><a href="#">SERVICES</a>
                         <ul class="submenu">
-                            <li><a href="#">ANUNCIAR CASAS EN ALQUILER</a></li>
-                            <li><a href="#">PODER RENTAR CASAS</a></li>
-                            <li><a href="#">CONTACTO ENTRE ALQUILADOR Y RENTADOR</a></li>
+                            <li><a href="#">Anunciar</a></li>
+                            <li><a href="#">Alquilar</a></li>
+                            <li><a href="#">Contactar</a></li>
                         </ul>
                     </li>
-                    <li><a href="#">CONTACTO</a></li>
-                    <li><a href="#">NOSOTROS</a></li>
-                    <li><a href="./views/Register.php">REGISTRO</a></li>
+                    <li><a href="#">LOG IN / REGISTRER</a>
+                        <ul class="submenu">
+                            <li><a href="#">Log In</a></li>
+                            <li><a href="/views/Register.php">Registrer</a></li>
+                        </ul>
+                    </li>
+
                 </ul>
                 </ul>
             </div>
         </nav>
         <!-- HEADER AND MENU -->
+    </header>
 
+    <!-- BANNER -->
+    <div class="banner">
+        <div class="banner-content">
+            <h1>¡House rent Armenia!</h1>
+            <h2>Armenia es el punto de partida ideal para adentrarse en la Colombia cafetera. Es una invitación a vivir una experiencia urbana en medio del más bello ambiente natural.
+                Visitar Armenia es una invitación para disfrutar de la naturaleza y la vida. En la capital del Departamento del Quindío existe una delicada amalgama entre lo rural y lo urbano.</h2>
 
-
-        <!-- CARDS HOUSES -->
-        <div class="contedor-tarjetas">
-
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-w1                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
-            <div class="card">
-                <img src="assests/img/photo1.jpg">
-                <h4>Casa Camprestre </h4>
-                <P>Agradable casa para hasta 5 huéspedes </P>
-                <span>
-                    <i class="fa fa-map-marker"></i>
-                    EJE CAFETERO, FINLANDIA
-                </span>
-                <span>
-                    <i class="fa fa-bed"></i> 3 Bedrooms
-                </span>
-                <span>
-                    <i class="fa fa-bath"></i>2 Bathroom
-                </span>
-                <div class="rent-price pull-left">$200</div>
-                <a href="#">VER MAS</a>
-            </div>
         </div>
+    </div>
+
+    <!-- CARDS HOUSES -->
+    <div class="contedor-tarjetas">
+
+
+        <div class="card">
+            <img src="assests/img/photo1.jpg">
+            <h4>Casa Camprestre </h4>
+            <P>Agradable casa para hasta 5 huéspedes </P>
+            <span>
+                <i class="fa fa-map-marker"></i>
+                EJE CAFETERO, FINLANDIA
+            </span>
+            <span>
+                <i class="fa fa-bed"></i> 3 Bedrooms
+            </span>
+            <span>
+                <i class="fa fa-bath"></i>2 Bathroom
+            </span>
+            <div class="rent-price pull-left">$200</div>
+            <a href="#">VER MAS</a>
+        </div>
+
+        <div class="card">
+            <img src="assests/img/photo1.jpg">
+            <h4>Casa Camprestre </h4>
+            <P>Agradable casa para hasta 5 huéspedes </P>
+            <span>
+                <i class="fa fa-map-marker"></i>
+                EJE CAFETERO, FINLANDIA
+            </span>
+            <span>
+                <i class="fa fa-bed"></i> 3 Bedrooms
+            </span>
+            <span>
+                <i class="fa fa-bath"></i>2 Bathroom
+            </span>
+            <div class="rent-price pull-left">$200</div>
+            <a href="#">VER MAS</a>
+        </div>
+        <div class="card">
+            <img src="assests/img/photo1.jpg">
+            <h4>Casa Camprestre </h4>
+            <P>Agradable casa para hasta 5 huéspedes </P>
+            <span>
+                <i class="fa fa-map-marker"></i>
+                EJE CAFETERO, FINLANDIA
+            </span>
+            <span>
+                <i class="fa fa-bed"></i> 3 Bedrooms
+            </span>
+            <span>
+                <i class="fa fa-bath"></i>2 Bathroom
+            </span>
+            <div class="rent-price pull-left">$200</div>
+            <a href="#">VER MAS</a>
+        </div>
+
+
+
+    </div>
+    <!-- Footer -->
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <!--Pulling Awesome Font -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <div class="col-md-12">
+        <div class="social">
+            <ul class="social-network social-circle">
+                <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+
+            <p class="copy">© The Best Team!</p>
+        </div>
+    </div>
+
+
+
 </body>
 
 </html>
