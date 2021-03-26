39d4bf1a0c5f7c2d934ed5b3bc179359ee0bbc9f
Paola Roa <yproab@gmail.com>
2021-03-26 16:03:23 -0500

    set changes on booking class

--- models/Booking.php
+++ models/Booking.php
@@ -1,10 +1,5 @@
 <?php 
-
-
 require_once("../utils/conexion.php");
-
-
-
 
 class Booking{
 
@@ -19,10 +14,10 @@ class Booking{
     }
 
     public function setBooking($start_d,$final_date, $users_idusers, $houseId){
-        $this->$start_dB = $start_d;
-        $this->$final_date = $final_date;
-        $this->$users_idusers = $users_idusers;
-        $this->$houseId = $houseId();
+        $this->start_d = $start_d;
+        $this->final_date = $final_date;
+        $this->users_idusers = $users_idusers;
+        $this->houseId = $houseId();
     }
     public function createBooking(){
         $stmt = Conexion::connect()->prepare("INSERT INTO Bookings('start_date', final_date, users_idusers, houseId) values($this->$start_d','$this->$final_date','$this->$users_idusers','$this->$houseId)");
@@ -34,7 +29,7 @@ class Booking{
         $stmt2 = Conexion::connect()->prepare("INSERT INTO houses_has_Bookings values($this->$houseId','$this->$idBookings)");    
     }
     public function setId($id){
-        $this->$idBookings = $id;
+        $this->idBookings = $id;
     }
 
 }
