0956da9da34743df028803b4bd872d38e702ace5
kevin dorado <kadoradob@gmail.com>
2021-03-25 19:18:43 -0500

    iniital class home

--- models/House.php
+++ models/House.php
@@ -4,4 +4,20 @@
 require_once("../utils/conexion.php");
 
 
+class House {
+    private $name;
+    private $description;
+    private $num_rooms;
+    private $num_toilets;
+    private $parking_lot;
+    private $internet;
+    private $price_pn;
+
+    public function __construct(){
+        
+    }
+
+}
+
+
 ?>
