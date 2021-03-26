ade8a4311b52635f2d51784bf6d7fb75ddc98a28
Juliana Fajardo <juliana.fajardo92@gmail.com>
2021-03-26 11:58:15 -0500

    index26032021

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 </head>
 
 <body>
+    <!-- HEADER AND MENU -->
     <header>
         <nav class="navbar">
             <div class="container">
@@ -27,12 +28,6 @@
                     <li><a href="#">Casas</a></li>
                     <li><a href="#">Contacto</a></li>
                     <li><a href="#">Nosotros</a>
-                        <ul class="submenu">
-                            <li><a href="#">Anunciar</a></li>
-                            <li><a href="#">Alquilar</a></li>
-                            <li><a href="#">Contactar</a></li>
-                        </ul>
-                    </li>
                     <li><a href="#">Inicia Sesión/Registrate</a>
                         <ul class="submenu">
                             <li><a href="./views/Login.php">ingresar</a></li>
@@ -44,83 +39,85 @@
                 </ul>
             </div>
         </nav>
-        <!-- HEADER AND MENU -->
-    </header>
+        <!-- HEADER AND MENU  END-->
 
-    <!-- BANNER 
-    <div class="banner">
-        <div class="banner-content">
-            <h1>¡House rent Armenia!</h1>
-            <h2>Armenia es el punto de partida ideal para adentrarse en la Colombia cafetera. Es una invitación a vivir una experiencia urbana en medio del más bello ambiente natural.
-                Visitar Armenia es una invitación para disfrutar de la naturaleza y la vida. En la capital del Departamento del Quindío existe una delicada amalgama entre lo rural y lo urbano.</h2>
 
+        <!-- BANNER -->
+        <div class="banner">
+            <div class="banner-content">
+                <h1>¡En House rent Armenia te damos la bienvenida !</h1>
+
+            </div>
         </div>
-    </div>-->
+        <!--BANNER END -->
 
-    <!-- CARDS HOUSES -->
-    <div class="contedor-tarjetas">
-
-
-        <div class="card">
-            <img src="assests/img/Parque-del-Cafe.jpg">
-            <h4>Parque del Café </h4>
-            <P>El Parque del Café es un parque temático colombiano situado en el corregimiento de Pueblo Tapao, del municipio de Montenegro en Quindío, Colombia. Dispone de 40 atracciones que se dividen en: mecánicas, acuáticas y temáticas. </P>
-            <span>
-                <i class="fa fa-map-marker"></i>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.297958972495!2d-75.7728367856766!3d4.540285244292901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e385ef825fa221d%3A0xd35336082b9ca116!2sParque%20del%20Caf%C3%A9!5e0!3m2!1ses-419!2sco!4v1616711556667!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </span>
-        </div>
+        <!-- CARDS TUR -->
+        <div class="contedor-tarjetas">
 
 
             <div class="card">
-            <img src="assests/img/MARIPOSARIO-QUINDIO.jpg">
-            <h4>Jardín Botanico del Quindio </h4>
-            <p>El Jardín Botánico del Quindío es un centro de investigación científica y de educación ambiental ubicado en la ciudad de Calarcá. Es el primer legalmente constituido en Colombia.</p>
-            <span>
-                <i class="fa fa-map-marker"></i>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15909.022041084676!2d-75.6603403!3d4.5479794!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x159c0ed6d96d65f!2sParque%20De%20La%20Vida!5e0!3m2!1ses-419!2sco!4v1616712211080!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </span>
+                <img src="assests/img/Parque-del-Cafe.jpg">
+                <h1>Parque del Café </h1>
+                <P>El Parque del Café es un parque temático colombiano situado en el corregimiento de Pueblo Tapao, del municipio de Montenegro en Quindío, Colombia. Dispone de 40 atracciones que se dividen en: mecánicas, acuáticas y temáticas. </P>
+                <span>
+                    <i class="fa fa-map-marker"></i>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.297958972495!2d-75.7728367856766!3d4.540285244292901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e385ef825fa221d%3A0xd35336082b9ca116!2sParque%20del%20Caf%C3%A9!5e0!3m2!1ses-419!2sco!4v1616711556667!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </span>
+            </div>
+
+
+            <div class="card">
+                <img src="assests/img/MARIPOSARIO-QUINDIO.jpg">
+                <h1>Jardín Botanico del Quindio </h1>
+                <p>El Jardín Botánico del Quindío es un centro de investigación científica y de educación ambiental ubicado en la ciudad de Calarcá. Es el primer legalmente constituido en Colombia.</p>
+                <span>
+                    <i class="fa fa-map-marker"></i>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15909.022041084676!2d-75.6603403!3d4.5479794!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x159c0ed6d96d65f!2sParque%20De%20La%20Vida!5e0!3m2!1ses-419!2sco!4v1616712211080!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </span>
+            </div>
+
+            <div class="card">
+                <img src="assests/img/parquedelavida.jpg">
+                <h1>Parque De La Vida</h1>
+                <p>El Parque de la Vida es el parque urbano más grande e importante de la ciudad de Armenia, Colombia.​ Se encuentra ubicado en el norte de la ciudad.</p>
+                <span>
+                    <i class="fa fa-map-marker"></i>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15909.820567866507!2d-75.6509772!3d4.5116806!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf97d58b928ca87fe!2zSmFyZMOtbiBCb3TDoW5pY28gRGVsIFF1aW5kw61v!5e0!3m2!1ses-419!2sco!4v1616711993719!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </span>
+            </div>
         </div>
-
-        <div class="card">
-            <img src="assests/img/parquedelavida.jpg">
-            <h4>Parque De La Vida</h4>
-            <p>El Parque de la Vida es el parque urbano más grande e importante de la ciudad de Armenia, Colombia.​ Se encuentra ubicado en el norte de la ciudad.</p>
-            <span>
-                <i class="fa fa-map-marker"></i>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15909.820567866507!2d-75.6509772!3d4.5116806!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf97d58b928ca87fe!2zSmFyZMOtbiBCb3TDoW5pY28gRGVsIFF1aW5kw61v!5e0!3m2!1ses-419!2sco!4v1616711993719!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </span>
-        </div>
+        <!-- CARDS TUR END-->
 
 
-    </div>
-    <!-- Footer -->
+        <!-- Footer -->
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <!------ Include the above in your HEAD tag ---------->
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
 
-    <!--Pulling Awesome Font -->
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-
-    <div class="col-md-12">
-        <div class="social">
-            <ul class="social-network social-circle">
-                <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
-                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-            </ul>
-
-            <p class="copy">© The Best Team!</p>
-        </div>
-    </div>
-
-
-
+        <!--Pulling Awesome Font -->
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <footer class="text-center text-white">
+            <!-- Grid container -->
+            <div class="container pt-4">
+                <!-- Section: Social media -->
+                <section class="mb-4">
+                    <div class="col-md-12">
+                        <div class="social">
+                            <ul class="social-network social-circle">
+                                <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+                                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                        <p>© 2020 Copyright:The Best Team!</p>
+                    </div>
+                </section>
+            </div>
+        </footer>
 </body>
 
 </html>
