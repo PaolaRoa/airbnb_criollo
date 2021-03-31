3a0f854007bb9a80c3dadd5c5a29753e736bf66b
Paola R <yproab@gmail.com>
2021-03-31 16:51:36 -0500

    insert total on booking table

--- controllers/BookingController.php
+++ controllers/BookingController.php
@@ -4,15 +4,51 @@
 require_once "../models/Booking.php";
 
 $action = $_GET['action'];
+switch ($action){
+    case "search":
+        search();
+        break;
+    case "book":
+        book();
+        break;
+}
 
-
-if ($action=='search'){
+function search(){
+    session_start();
+    $_SESSION['start_date']= $_POST['start-date'];
+    $_SESSION['end_date']= $_POST['ending-date'];
+    header('Location: ../views/Lesseegalery.php');
+}
+function book(){
+    session_start();
+    $start = $_SESSION['start_date'];
+    $end = $_SESSION['end_date'];
+    $idhouse = $_GET['id'];
+    $iduser = $_SESSION['iduser'];
+    $booking = new Booking();
+    $booking->setBooking($start, $end, $iduser, $idhouse);
+    $test = $booking->setTotal($idhouse);
+    $resp = $booking->createBooking();
+    if($resp){
+        echo '<script type="text/javascript">
+        alert("Registro exitoso");
+        window.location.href="../index.php";
+        </script>';
+    }
+    else{
+        echo'<script type="text/javascript">
+        alert("no se pudo reservar");
+        window.location.href="../index.php";
+        </script>';
+    }
+}
+/*if ($action=='search'){
     session_start();
     $_SESSION['start_date']= $_POST['start-date'];
     $_SESSION['end_date']= $_POST['ending-date'];
     header('Location: ../views/Lesseegalery.php');
     /*echo $_SESSION['start_date'];
-    echo $_SESSION['end_date'];*/
+    echo $_SESSION['end_date'];
 
 }
 else if ($action=='book'){
@@ -22,10 +58,12 @@ else if ($action=='book'){
     $idhouse = $_GET['id'];
     $iduser = $_SESSION['iduser'];
     $booking = new Booking();
-    $booking->setBooking($start, $end, $iduser, $iduser);
+    $booking->setBooking($start, $end, $iduser, $idhouse);
+    $test = $booking->setTotal($idhouse);
     $resp = $booking->createBooking();
-    if($resp){
-        echo'<script type="text/javascript">
+
+    if(true){
+        '<script type="text/javascript">
         alert("Registro exitoso");
         window.location.href="../index.php";
         </script>';
@@ -39,7 +77,7 @@ else if ($action=='book'){
 
 
 
-}
+}*/
 
 
 
--- models/Booking.php
+++ models/Booking.php
@@ -8,6 +8,8 @@ class Booking{
     private $final_date;
     private $users_idusers;
     private $houseId;
+    //aqui voy
+    private $total;
 
     public function __construct(){
 
@@ -22,9 +24,7 @@ class Booking{
     }
     public function createBooking(){
         $con = Conexion::connect();
-        $stmt = $con->prepare("INSERT INTO bookings (`start_date`, final_date, users_idusers, houseId) values('$this->start_d', '$this->final_date',
-         '$this->users_idusers', '$this->houseId')");
-
+        $stmt = $con->prepare("INSERT INTO bookings (`start_date`, final_date, users_idusers, houseId, total) values('$this->start_d', '$this->final_date','$this->users_idusers', '$this->houseId', '$this->total')");
         $resp = $stmt->execute();
         //set the id that was assign in the last query
         $id=$con->lastInsertId();
@@ -35,8 +35,20 @@ class Booking{
     public function setId($id){
         $this->idBookings = $id;
     }
-    /*public function get_avalaiblehouses($start_d, $end_date){
-        $stmt = Conexion::connect()prepare("SELECT houseId from Bookings");
-    }*/
+    public function setTotal($idHouse){
+        //calc days with start and end date
+        $date1 = new DateTime($this->start_d);
+        $date2 = new DateTime($this->final_date);
+        $diff = $date1->diff($date2);
+        $days = $diff->days;
+        $con = Conexion::connect();
+        $stmt = $con->prepare("SELECT price_pn FROM houses where  idhouses='$idHouse'");
+        $stmt->execute();
+        $price = $stmt->fetchColumn();
+        //calc total
+        $total = $price * $days;
+        $this->total = $total;   
+    }
+
 
 }
