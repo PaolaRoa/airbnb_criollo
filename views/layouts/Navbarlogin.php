f259608d3679dd67a8fc6d5620a12f8bf97b175f
Juliana Fajardo <juliana.fajardo92@gmail.com>
2021-04-06 15:51:55 -0500

    changeresponsivehelp

--- views/layouts/Navbarlogin.php
+++ views/layouts/Navbarlogin.php
@@ -1,24 +1,23 @@
 <?php
-    session_start();
+session_start();
 ?>
-    <header>
-        <a href="../index.php">
-            <img src="../assests/img/hotel2.png" />
-        </a>
-        <input type="checkbox" id="btn-menu">
-        <label for="btn-menu"><i class="fas fa-bars"></i></label>
-        <nav class="menu">
-            <ul>
-                <li><a href="../views/Register.php" ><i class="fas fa-user-plus"></i>
+<header>
+    <a href="../index.php">
+        <img src="../assests/img/hotel2.png" />
+    </a>
+    <input type="checkbox" id="btn-menu">
+    <label for="btn-menu"><i class="fas fa-bars"></i></label>
+    <nav class="menu">
+        <ul>
+            <li><a href="../views/Register.php"><i class="fas fa-user-plus"></i>
+                    Registrarse</a></li></a></li>
+                    
+            <li><a href="#contacto"><i class="fas fa-question-circle"></i>
+                    Contacto</a></li></a></li>
 
+            <li><a href="../views/Help.php"><i class="fas fa-tools"></i>
+                    Ayuda</a></li></a></li>
 
-                      Registrarse</a></li></a></li>
-                <li><a href="#contacto" ><i class="fas fa-question-circle"></i>
-                      Contacto</a></li></a></li>
-                
-                <li><a href="../views/Help.php" ><i class="fas fa-tools"></i>
-                        Ayuda</a></li></a></li>
-               
-            </ul>
-        </nav>
-    </header>
+        </ul>
+    </nav>
+</header>
