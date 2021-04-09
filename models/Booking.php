3badc4e6bc0afad0880d541913c173ee7d8ddd3d
Paola Roa <yproab@gmail.com>
2021-04-09 15:59:06 -0500

    set images on bookings view

--- models/Booking.php
+++ models/Booking.php
@@ -69,10 +69,12 @@ class Booking{
     }
     public function getUserBookings($iduser){
         $con = Conexion::connect();
-        $stmt = $con->prepare("SELECT h.*, b.total, b.start_date, b.final_date, b.idBookings FROM 
-        houses h, Bookings b
+        $stmt = $con->prepare("SELECT h.*, b.total, b.start_date, b.final_date, b.idBookings, i.url FROM 
+        houses h, Bookings b, images i
         where h.idhouses = b.housesId
-        and b.users_idusers =$iduser");
+        and h.idhouses = i.houses_idhouses
+        and i.main = 1
+        and b.users_idusers =$iduser;");
         $stmt->execute();
         $arrBookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $arrBookings;
