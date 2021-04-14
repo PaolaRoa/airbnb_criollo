e41a4e69fd40cc348860ae23275ee212ebc726cd
kevin dorado <kadoradob@gmail.com>
2021-04-13 23:11:06 -0500

    verificar reload

--- assests/js/imageHouse.js
+++ assests/js/imageHouse.js
@@ -1,4 +1,24 @@
-function img(smaLLImg){
-    var fuLLImg = document.getElementById("image-box");
-    fuLLImg.src = smaLLImg.src;
+function img(smaLLImg) {
+  var fuLLImg = document.getElementById("image-box");
+  fuLLImg.src = smaLLImg.src;
+}
+
+function editServices(id) {
+  let url = "../controllers/ServicesController.php";
+  let formdata = new FormData();
+
+  formdata.append("operationHouse", "editservice");
+  formdata.append("id", id);
+
+  fetch(url, {
+    method: "post",
+    body: formdata,
+  })
+    .then((data) => data.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.error(error)
+    });
 }
---
+++ controllers/ServicesController.php
@@ -0,0 +1,59 @@
+
+
+<?php ob_start() ?>
+<?php
+
+require_once "../models/AditionalServerHelp.php";
+
+session_start();
+
+
+
+if (isset($_POST["operationHouse"])){
+    $id_house = $_POST["id"]; 
+    $operation = $_POST['operationHouse'];
+    $id_lessor=  $_SESSION['iduser'];
+
+   switch($operation){
+      case 'editservice':
+         $servicesObj = new AditionalServerHelp();
+         $uservices = $servicesObj->getHouseServices($id_house);
+
+         echo json_encode($uservices);
+         break;
+
+         case 'update_services':
+            $id_house = $_POST["id"];
+            $piscina = $_POST["piscina"];
+            $limpieza = $_POST["limpieza"];
+            $aire = $_POST["aire"];
+            $agua = $_POST["agua"];
+            $sauna = $_POST["sauna"];
+
+
+            $servicesObje = new AditionalServerHelp();
+            $result =$servicesObje->deleteService($id_house);
+
+            $services = array($piscina, $limpieza, $aire, $agua, $sauna);
+
+           
+            foreach($services as $servi )
+            {
+               if($servi != NULL){
+                  $servicesObje->setService($servi);
+                  $idservice =$servicesObje->idService();
+                 
+                  // set for add service
+                  $servicesObje->setServiceHelp($id_house, $idservice["idadditional_services"]);
+                  $servicesObje->createServiceHelp();
+               }
+            }
+
+            echo json_encode($servicesObje->getHouseServices($id_house));
+            break;
+
+         default:
+         break;
+   }
+
+}
