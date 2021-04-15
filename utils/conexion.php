b4feeb5367b6dbc974626e2c09c2e58844ddc6fa
Paola Roa <yproab@gmail.com>
2021-04-14 19:01:08 -0500

    change connection file

--- utils/conexion.php
+++ utils/conexion.php
@@ -8,11 +8,13 @@
     
     $db = "heroku_1db393ffc316d6b";
 
+    //mysql:host=127.0.0.1;port=3306;dbname=mysql;charset=utf8','user','password'//
+
 class Conexion{
 
 public static function connect(){
 
-    $link = new PDO("mysql:host=$servername; dbname=$db", $username, $password,
+    $link = new PDO("mysql:host=us-cdbr-east-03.cleardb.com; dbname=heroku_1db393ffc316d6b", 'b929cd0fbe4485', 'e5cdb617',
                     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                           PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
                     );
